77445e35bf06ed79e8fd4da9cb2a18fc33e7d176
Navin balekomebole <navin.balekomebole@stu.ocsb.ca>
2023-02-08 19:29:46 +0000

    added helloworldstyle

---
+++ HelloWorldStyle.java
@@ -0,0 +1,19 @@
+/**
+* The HelloWorld program implements an application that simply
+* displays "Hello world!" to the standard output.
+*
+* @author  Mr. Coxall
+* @version 1.0
+* @since   2021-11-16
+*/
+class HelloWorldStyle {
+    /**
+    * Main function.
+    *
+    * @param args Unused
+    */
+    public static void main(String[] args) {
+        System.out.println("Next year, I plan on going to university.");
+        System.out.println("I feel pretty good about moving on to university.");
+    }
+}
